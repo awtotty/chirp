eec28dd69c5adb8be724ce0149c871299112433a
Austin <awtotty@gmail.com>
2023-11-04 09:23:18 -0700

    Posts and create wizard

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,6 +1,14 @@
+import { clerkClient } from "@clerk/nextjs";
+import type { User } from "@clerk/nextjs/dist/types/server";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+// client should only see limited info about a user for rendering posts
+const filterUserForClient = (user: User) => {
+  return { id: user.id, username: user.username, imgUrl: user.imageUrl }
+};
 
 export const postRouter = createTRPCRouter({
   getLatest: publicProcedure.query(({ ctx }) => {
@@ -10,7 +18,12 @@ export const postRouter = createTRPCRouter({
   }),
 
   // define a route endpoint and what it should do 
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.db.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.db.post.findMany({
+      take: 100,
+      orderBy: { createdAt: "desc" },
+    }); 
+
+    return posts;
   }),
 });
